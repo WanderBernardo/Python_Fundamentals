a9231a47c9bc9c8a0475f95c8e8e621289fa1444
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-07 20:32:41 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Input_Print.py
@@ -0,0 +1,48 @@
+
+
+# Execute each code in interative mode
+
+
+# Test 01
+name = input("Provide your name: ")
+
+print(name)
+
+
+# Test 02
+name = input("Provide your name: ")
+age = input("What's your age: ")
+
+
+print(name, age)
+
+
+
+# Test 03
+
+name = input("Provide your name: ")
+age = input("What's your age: ")
+
+
+print(name, age, sep="#")
+
+
+# Test 04
+
+name = input("Provide your name: ")
+age = input("What's your age: ")
+city = input("What's your city: ")
+
+print(name, age, sep="#")
+print(city)
+
+
+
+#Test 05
+name = input("Provide your name: ")
+age = input("What's your age: ")
+city = input("What's your city: ")
+
+print(name, age, end="... \n")
+print(city)
+ 
