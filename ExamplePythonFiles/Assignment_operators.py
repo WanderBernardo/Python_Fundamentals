f7a74358f37d154f1b76876ddde9e1c06075c83a
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-09 10:19:01 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Assignment_operators.py
@@ -0,0 +1,32 @@
+#Use interative Mode
+
+]a = 10
+
+a += 5   # Equivalent to a = a + 5
+print(a) # 15
+
+a -= 3   # Equivalent to a = a - 3
+print(a) # 12
+
+a *= 2   # Equivalent to a = a * 2
+print(a) # 24
+
+a /= 4   # Equivalent to a = a / 4
+print(a) # 6.0
+
+a %= 3   # Equivalent to a = a % 3
+print(a) # 0.0
+
+a **= 2  # Equivalent to a = a ** 2
+print(a) # 0.0
+
+a //= 2  # Equivalent to a = a // 2
+print(a) # 0.0
+
+
+
+# Imagine You add R$ 10 in the account bank and your account there was R$ 20 then, total actual: R$ 30:
+
+Account = 20
+Account += 10
+print(Account)
