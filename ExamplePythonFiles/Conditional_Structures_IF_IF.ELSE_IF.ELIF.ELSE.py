6da5d01b393733fd0da8e741b1d9715550770b2b
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-12 19:31:40 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Conditional_Structures_IF_IF.ELSE_IF.ELIF.ELSE.py
@@ -0,0 +1,26 @@
+# Use interative mode
+
+# IF
+number = 10
+
+if number > 5:
+    print("number is greater than 5.")
+
+
+# IF ELSE
+age = 16
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+# IF ELIF ELSE
+light_color = "green"  # This can be "green", "yellow", or "red"
+
+if light_color == "green":
+    print("Go")
+elif light_color == "yellow":
+    print("Prepare to Stop")
+else:
+    print("Stop")
