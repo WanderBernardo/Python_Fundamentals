e93c0bb7580710833cda245118ade8abe02a9475
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-17 07:25:12 -0300

    Update Repetition_Structure_FOR.py

--- ExamplePythonFiles/Repetition_Structure_FOR.py
+++ ExamplePythonFiles/Repetition_Structure_FOR.py
@@ -19,3 +19,19 @@ a = int(input("Inform an integer number: "))
 for _ in range(2):
     a += 1
     print(a)
+
+
+
+# FOR ELSE
+
+# Checking username availability using for-else
+
+registered_users = ['alice', 'bob', 'carol', 'dave']
+new_user = 'eve'
+
+for user in registered_users:
+    if user == new_user:
+        print(f'The username "{new_user}" is already taken. Please choose another one.')
+        break
+else:
+    print(f'The username "{new_user}" is available!')
