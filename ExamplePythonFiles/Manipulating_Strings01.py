92ceeff03d605785b1ba8f0f2ced87eda3e4dd51
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-17 16:53:45 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Manipulating_Strings01.py
@@ -0,0 +1,25 @@
+
+
+# Using Upper, Lower and title
+curso = "pYtHon"
+
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+
+# Eliminating white space
+
+curso = "   Python "
+
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+
+
+# Separate and centralize
+curso = "Python"
+
+print(curso.center(10, "#"))
+print(".".join(curso))
+
