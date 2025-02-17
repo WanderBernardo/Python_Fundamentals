0141ab9a51619826952fc4b2377d56b684e53d34
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-17 10:13:28 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Range.py
@@ -0,0 +1,17 @@
+# Use interative mode
+
+# Argument: Stop
+for i in range(5):
+    print(i)
+# Output: 0, 1, 2, 3, 4
+
+
+# ARguments: Start and Stop
+for i in range(2, 6):
+    print(i)
+# Output: 2, 3, 4, 5
+
+# Arguments: Start, Stop and Step
+for i in range(1, 10, 2):
+    print(i)
+# Output: 1, 3, 5, 7, 9
---
+++ ExamplePythonFiles/Repetation_Structure_WHILE.py
@@ -0,0 +1,15 @@
+# Use interative Mode
+
+count = 1
+while count <= 5:
+    print(f'Number: {count}')
+    count += 1
+
+
+
+count = 1
+while count <= 5:
+    print(f'Number: {count}')
+    count += 1
+else:
+    print('The loop has finished.')
