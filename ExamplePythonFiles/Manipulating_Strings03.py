4cfb51334042e80692784a6ccba9f86363b88e76
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-24 17:25:20 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Manipulating_Strings03.py
@@ -0,0 +1,25 @@
+
+#Use interative Mode
+
+name = "Diana Floriano Silva"
+
+# Example 01: Variable name = [start]
+name[2]
+
+# Example 02: Variable name = [:stop]
+name[:10]
+
+# Example 03: Variable name = [start:]
+name[10:]
+
+# Example 04: Variable name = [start:stop]
+name[5:10]
+
+# Example 05: Variable name = [start:stop:step]
+name[5:10:2]
+
+# Example 06: Variable name = [:]
+name[:]
+
+# Example 07: Variable name = [::-step]
+name[::-1]
