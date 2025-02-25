db1b67ac25680e0fa19308cbf288b2cb989061d0
Wanderley Bernardo da Silva <wanderley.bernardo@gmail.com>
2025-02-25 09:15:33 -0300

    Add files via upload

---
+++ ExamplePythonFiles/Triples_Strings.py
@@ -0,0 +1,11 @@
+name = "Wanderley"
+menu = f'''
+################ MENU of the {name} #############
+
+[1] Drinks
+[2] Special Dishes
+[3] Desserts
+
+###################################################
+'''
+print(menu)
